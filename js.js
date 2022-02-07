360b6dfbcba2ac7e1d7a533c877fdd51ad7ee157
RinadAkel <70144208+RinadAkel@users.noreply.github.com>
2022-02-07 13:27:46 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 500) {
+        $("#navBar").addClass("noTransparrent");
+    }
+    else {
+        $("#navBar").removeClass("noTransparrent");
+    }
+});
